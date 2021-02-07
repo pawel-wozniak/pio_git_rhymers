bf2a13da8b7f8142a3a950aee9fe515b8416b340
Pawel_W <kontakt@pawelwozniak.pl>
2021-02-07 23:36:14 +0100

    1.5.6 Add Javadoc for FIFORhymer class

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -2,8 +2,15 @@ package edu.kis.vh.nursery;
 
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
+	/**
+	 * Field containing DefaultCountingOutRhymer
+	 */
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
+	/**
+	 * Overriding method from DefaultCountingOutRhymer
+	 * @return current value
+	 */
     @Override
     public int countOut() {
 		while (!callCheck()) {
