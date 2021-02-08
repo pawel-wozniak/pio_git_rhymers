3c6b20fdb3fc50c99a2195ca6fb4293fbea0c25f
Pawel_W <kontakt@pawelwozniak.pl>
2021-02-08 13:02:52 +0100

    1.5.4 Update IntLinkedListTest, add test for top() method

--- src/test/java/edu/kis/vh/nursery/IntLinkedListTest.java
+++ src/test/java/edu/kis/vh/nursery/IntLinkedListTest.java
@@ -12,6 +12,7 @@ public class IntLinkedListTest {
         Assert.assertTrue(list.isEmpty());
         list.push(1);
         Assert.assertFalse(list.isEmpty());
+        Assert.assertEquals(1, list.top());
         int result = list.pop();
         Assert.assertTrue(list.isEmpty());
         Assert.assertEquals(1, result);
