e4aee18cb167dc61f25a37673360295138edc935
Pawel_W <kontakt@pawelwozniak.pl>
2021-02-08 13:00:29 +0100

    1.5.4 Add IntLinkedListTest

---
+++ src/test/java/edu/kis/vh/nursery/IntLinkedListTest.java
@@ -0,0 +1,19 @@
+package edu.kis.vh.nursery;
+
+import edu.kis.vh.nursery.list.IntLinkedList;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IntLinkedListTest {
+
+    @Test
+    public void testNewIntLinkedList() {
+        IntLinkedList list = new IntLinkedList();
+        Assert.assertTrue(list.isEmpty());
+        list.push(1);
+        Assert.assertFalse(list.isEmpty());
+        int result = list.pop();
+        Assert.assertTrue(list.isEmpty());
+        Assert.assertEquals(1, result);
+    }
+}
